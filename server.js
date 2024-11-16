ec79822ae072cca2c510a6f1dfe35e99d5e0134d
hBilal-Zia <hbilalzia92@gmail.com>
2024-11-16 19:14:33 +0500

    updated Server.js

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const { createServer } = require('node:http');
+const { verifyToken } = require('./middlewears/checkToken')
+const {setupSocket} = require('./socket')
+const path = require('path');
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-	res.status(200).send({message: "Server is Up!"})
-})
+const PORT = process.env.PORT || 8080;
+const app = express();
+const server = createServer(app);
 
-app.listen(8080, () => {
-	console.log('Server is listenign at port 8080')
-})
+app.use(cors({
+    origin: '*',
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
+app.use(express.json());
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+setupSocket(server)
+
+app.get('/health', (req, res) => {
+    res.status(200).send({ "message": "Server is Up" });
+});
+
+
+app.use('/', require('./routes/viewsRoutes'))
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/books', verifyToken, require('./routes/bookRoutes'));
+app.use('/api/genres', require('./routes/genreRoutes'));
+app.use('/api/users', verifyToken, require('./routes/userRoutes'))
+
+server.listen(PORT, () => {
+    console.log(`Server is listening at Port = ${PORT}`);
+});
